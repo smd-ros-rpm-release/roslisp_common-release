bcd4dccad7ea953e1e37ebc1dd98a8e0159ab3c1
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2014-12-01 08:43:38 +0100

    Rebase from 'debian/hydro/cl_transforms'

--- src/package.lisp
+++ src/package.lisp
@@ -4,7 +4,7 @@
            :x :y :z :w :make-quaternion :make-identity-rotation
            :copy-quaternion :q= :q* :q-inv :q+ :q- :q-norm :q-dot :q-scale
            :squared-norm :3d-vector :make-3d-vector :copy-3d-vector
-           :make-identity-vector :v+ :v- :v* :v-inv :dot-product
+           :make-identity-vector :v+ :v- :v* :v-inv :dot-product :normalize-vector
            :cross-product :v-dist :v-norm :rotate :axis-angle->quaternion
            :quaternion->axis-angle :yaw :get-yaw
            :euler->quaternion :matrix->quaternion :normalize
@@ -15,6 +15,8 @@
            :point :pose :make-pose :make-identity-pose :copy-pose
            :make-2d-pose :transform-pose :reference-transform :origin :orientation
            :transform->pose :pose->transform
-           :matrix->quaternion :matrix->transform :transform->matrix :pose->matrix
-           :slerp :interpolate-vector))
-
+           :matrix->quaternion :quaternion->matrix :matrix->transform :transform->matrix 
+           :pose->matrix :slerp :interpolate-vector :invert-rot-matrix :transpose-rot-matrix
+           :transpose-2d-matrix :column-vectors->quaternion
+           :twist :make-twist :make-identity-twist :copy-twist
+           :wrench :make-wrench :make-identity-wrench :copy-wrench))
